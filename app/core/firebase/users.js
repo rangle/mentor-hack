00e24fee2629a16da6a61a639f01da990bfed53d
Abdella Ali <abdella@rangle.io>
2014-04-25 18:11:22 -0400

    create rough users service

--- app/core/firebase/users.js
+++ app/core/firebase/users.js
@@ -1,3 +1,14 @@
 /**
  * Created by della on 2014-04-25.
  */
+
+angular.module('app')
+.service('Users', [function () {
+}])
+
+  .factory('unTaggedStore',['$firebase', function($firebase){
+
+    var ref = new Firebase('');
+
+    return $firebase(ref);
+  }]);
