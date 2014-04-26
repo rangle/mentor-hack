e4f97e8aa478b18107c8d1198a88448e0bd7a9d3
Yuri Takhteyev <yuri@freewisdom.org>
2014-04-25 20:55:30 -0400

    Adding a client-side "server" service + fixing some code broken by the move from firebase.

---
+++ app/core/server.js
@@ -0,0 +1,35 @@
+/* global angular */
+
+angular.module('app.server', [
+  'koast'
+])
+
+.factory('server', ['koast',
+  function(koast) {
+    var service = {};
+    var promiseArray = [];
+
+    ['users', 'teams'].forEach(function(endpoint) {
+      var promise = koast.queryForResources(endpoint)
+        .then(function(users) {
+          console.log('Users', users);
+          service.users = users;
+        })
+        .then(null, $log.error);
+      });
+
+    service.whenReady = function() {
+      return $q.all(promiseArray);
+    };
+
+    return service;
+  }
+])
+
+.run(['koast',
+  function(koast) {
+    koast.setApiUriPrefix('/api/');
+    koast.addEndpoint('users');
+    koast.addEndpoint('teams');
+  }
+]);
