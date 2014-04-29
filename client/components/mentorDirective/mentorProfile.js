a0b49bf01fcd11736c3b07e559a15d8060f3ab7a
Abdella Ali <abdella@rangle.io>
2014-04-29 16:47:58 -0400

    quick update for bower related installation

--- client/components/mentorDirective/mentorProfile.js
+++ client/components/mentorDirective/mentorProfile.js
@@ -5,7 +5,7 @@
 
 angular.module('app.components.directives', [])
 
-.directive('mentorProfile', function(server){
+  .directive('mentorProfile', function () {
 
     return{
       restrict: 'A',
@@ -14,41 +14,42 @@ angular.module('app.components.directives', [])
         'mentor': '=mentorProfile',
         'teams': '=teams'
       },
-      link: function(scope, element, attrs){
+      link: function (scope, element, attrs) {
 
-        scope.bookings = ['','','','','','','','',''];
+        scope.bookings = ['', '', '', '', '', '', '', '', ''];
 
         //Iterates over each booking currently on the mentor, and if not null (null means this spot is empty)
         //apply the booking to the overall scope.bookings, which is bound to the view
-        scope.mentor.bookings.forEach(function(bookingSlot, idx){
-          if(bookingSlot){
+        scope.mentor.bookings.forEach(function (bookingSlot, idx) {
+          if (bookingSlot) {
             scope.bookings[idx] = bookingSlot;
           }
         });
 
         scope.time = function (slot) {
-          var time = 9 + 1 * slot+1;
+          var time = 9 + 1 * slot + 1;
           time = time > 12 ? time + ' pm' : time + ' am';
           return time;
         };
 
         scope.taken = function (team) {
-          if(team === '') return;
-          else if(['Lunch', 'Dinner'].indexOf(team) >= 0) return 'break';
+          if (team === '') return;
+          else if (['Lunch', 'Dinner'].indexOf(team) >= 0) return 'break';
           else return 'taken';
         };
 
         // on-drop-success="dropSuccessHandler($event,$index,bookings)"
-        scope.dropSuccessHandler = function($event, index, array) {
+        scope.dropSuccessHandler = function ($event, index, array) {
           // array.splice(index, 1);
-//          server.users.push()
         };
 
-        console.log(server);
-        scope.onDrop = function($event, index, $data) {
-          if(scope.bookings[index] === '')
+        scope.onDrop = function ($event, index, $data) {
+          if (scope.bookings[index] === '') {
             scope.bookings[index] = $data.displayName;
-          else {
+            scope.mentor.bookings[index] = $data.displayName;
+            scope.mentor.save()
+              .then(null, $log.error);
+          } else {
             var r = confirm('This is slot is already taken, do you want to overwrite it?');
             if (r)
               scope.bookings[index] = $data.displayName;
