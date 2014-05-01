342e2d77e0452d916c7d76d2eb78269ef3a39533
Nitin Vericherla <nitin.vericherla@gmail.com>
2014-05-01 19:05:39 -0400

    imageMagick and upload to S3

--- server/lib/api.js
+++ server/lib/api.js
@@ -6,6 +6,7 @@
 var koast = require('koast');
 var connection = koast.getDatabaseConnectionNow();
 var mapper = koast.makeMongoMapper(connection);
+var imageUpload = require('./image-upload.js');
 
 
 
@@ -25,90 +26,5 @@ exports.routes = [
   ['post', 'teams', mapper.post('teams')],
   ['del', 'users/:_id', mapper.del('users')],
   ['del', 'teams/:_id', mapper.del('teams')],
-	['post', 'img-upload', function (req, resp) {
-      var fs = require('fs'),
-        gm = require('gm'),
-        _ = require("underscore"),
-        path = require("path"),
-        AWS = require("aws-sdk"),
-        imageMagick = gm.subClass({ imageMagick: true });
-
-      _.each(req.files, function (val, key) {
-        var tmp_path = val.path,
-          hashName = [tmp_path.substring(tmp_path.lastIndexOf('/') + 1), '640x470'].join(''),
-          target_path = path.resolve(__dirname, '../img/', hashName);
-
-        imageMagick(tmp_path).autoOrient().size(function (err, origSize) {
-          if (err) return console.error(err);
-
-          var newHeight = (470 * origSize.height) / origSize.width,
-            offsetH = (newHeight / 2) - 235;
-
-          imageMagick(tmp_path).
-            autoOrient().
-            resize(640).
-            stream(function (err, stdout, stderr) {
-              imageMagick(stdout).gravity("Center").crop(640, 470).write(target_path, function (err) {
-                if (err) return console.error(err);
-                console.log("resizing done");
-
-                fs.readFile(target_path, function (err, data) {
-                  if (err) return console.error(err);
-
-
-                  var awsConfig = koast.getConfig('aws'),
-                  		makeKey = function (req) {
-										   var fileName = req.files.attachment.file.originalFilename;
-										   fileName = moment.utc().format('YYYY-MM-DD-hh-mm-ss-') + fileName;
-										   return 'uploadedImages/' + fileName;
-										 };
-
-									AWS = require('aws-sdk');
-							    AWS.config.loadFromPath(awsConfig.configFile);
-							    s3 = new AWS.S3();
-
-							    var file = req.files.attachment.file;
-							    		key = makeKey(req);
-							    		params = {
-									      Bucket: awsConfig.bucket,
-									      Key: key,
-									      Body: data,
-									      ACL: 'public-read',
-									      ContentType: file.type
-									    };
-
-							    s3.putObject(params, function (err, awsResponse) {
-							      if (err) {
-							        throw err;
-							      } else {
-
-	                    console.log("S3 success!", awsResponse);
-	                    console.timeEnd("s3upload");
-
-	                    // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
-	                    fs.unlink(tmp_path, function () {
-	                      if (err) throw err;
-
-	                      fs.unlink(target_path, function () {
-	                        if (err) throw err;
-	                        res.send(200, {
-											      file: {
-											        url: 'https://' + awsConfig.bucket + '.s3.amazonaws.com/' + key
-											      }
-											   	});
-	                      });
-	                    });
-							      }
-							    });
-
-                });
-              });
-
-            });
-        });
-
-
-      });
-    }
-  ]
+	['post', 'img-upload', imageUpload.upload]
 ];
--- server/lib/image-upload.js
+++ server/lib/image-upload.js
@@ -1,72 +1,111 @@
-/**
- * Created by della on 2014-05-01.
- */
+exports.upload = function (req, resp) {
+  var fs = require('fs'),
+    gm = require('gm'),
+    _ = require("underscore"),
+    path = require("path"),
+    AWS = require("aws-sdk"),
+    koast = require("koast"),
+    imageMagick = gm.subClass({ imageMagick: true });
 
-exports.routes = [
-  ['post', 'img-upload', function (req, resp) {
-      var fs = require('fs'),
-        gm = require('gm'),
-        imageMagick = gm.subClass({ imageMagick: true });
+  _.each(req.files, function (val, key) {
+    var tmp_path = val.path,
+      hashName = [tmp_path.substring(tmp_path.lastIndexOf('/') + 1), '640x470'].join(''),
+      target_path = path.resolve(__dirname, '../img/', hashName);
 
-      _.each(req.files, function (val, key) {
-        var tmp_path = val.path,
-          hashName = [tmp_path.substring(tmp_path.lastIndexOf('/') + 1), '640x470'].join(''),
-          target_path = [rootDir, '/userassets/img/tmp/', hashName].join('');
+    imageMagick(tmp_path).autoOrient().size(function (err, origSize) {
+      if (err) return console.error(err);
 
-        imageMagick(tmp_path).autoOrient().size(function (err, origSize) {
-          if (err) return console.error(err);
+      var newHeight = (470 * origSize.height) / origSize.width,
+        offsetH = (newHeight / 2) - 235;
 
-          var newHeight = (470 * origSize.height) / origSize.width,
-            offsetH = (newHeight / 2) - 235;
+      imageMagick(tmp_path).
+        autoOrient().
+        resize(640).
+        stream(function (err, stdout, stderr) {
+          imageMagick(stdout).gravity("Center").crop(640, 470).write(target_path, function (err) {
+            if (err) return console.error(err);
+            console.log("resizing done");
 
-          imageMagick(tmp_path).
-            autoOrient().
-            resize(640).
-            stream(function (err, stdout, stderr) {
-              imageMagick(stdout).gravity("Center").crop(640, 470).write(target_path, function (err) {
-                if (err) return console.error(err);
-                console.log("resizing done");
+            fs.readFile(target_path, function (err, data) {
+              if (err) return console.error(err);
 
-                fs.readFile(target_path, function (err, data) {
-                  if (err) return console.error(err);
-                  console.time("s3upload");
-                  var s3 = new AWS.S3({params: {Bucket: 'MerchApp'}});
-                  console.log("Starting to send to S3");
-                  s3.putObject({
-                    Key: hashName,
+              var awsConfig = koast.getConfig('aws'),
+                  makeKey = function (req) {
+                   var fileName = val.originalFilename;
+                   //fileName = moment.utc().format('YYYY-MM-DD-hh-mm-ss-') + fileName;
+                   return 'uploadedImages/' + fileName;
+                 };
+
+              AWS = require('aws-sdk');
+              AWS.config.loadFromPath(awsConfig.configFile);
+              s3 = new AWS.S3();
+
+              var file = val;
+                  key = makeKey(req);
+                  params = {
+                    Bucket: awsConfig.bucket,
+                    Key: key,
                     Body: data,
-                    ACL: 'public-read'
-                  }, function (err, awsResponse) {
-                    if (err) return console.error(err);
+                    ACL: 'public-read',
+                    ContentType: file.type
+                  };
 
-                    console.log("S3 success!", awsResponse);
-                    console.timeEnd("s3upload");
+              s3.putObject(params, function (err, awsResponse) {
+                if (err) {
+                  throw err;
+                } else {
+                  console.log("S3 success!", awsResponse);
+                  console.timeEnd("s3upload");
 
-                    // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
-                    fs.unlink(tmp_path, function () {
+                  // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+                  fs.unlink(tmp_path, function () {
+                    if (err) throw err;
+
+                    fs.unlink(target_path, function () {
                       if (err) throw err;
-
-                      fs.unlink(target_path, function () {
-                        if (err) throw err;
-                        return Utils.writeJSON(
-                          {
-                            "msg": ['File uploaded', ' (', val.size, ' bytes).'].join(''),
-                            "url": ['https://s3.amazonaws.com/MerchApp/', hashName].join(''),
-                            "code": 200
-                          },
-                          resp
-                        );
+                      res.send(200, {
+                        file: {
+                          url: 'https://' + awsConfig.bucket + '.s3.amazonaws.com/' + key
+                        }
                       });
                     });
                   });
-                });
+                }
               });
 
             });
+          });
+
         });
+    });
 
 
-      });
+  });
+}
+
+/*exports.upload = function (req, resp) {
+  var fs = require('fs'),
+    _ = require("underscore"),
+    path = require("path"),
+    AWS = require("aws-sdk"),
+    koast = require("koast");
+
+  var awsConfig = koast.getConfig('aws-config');
+  var imageUploader = koast.makeS3FileUploader({
+    configFile: awsConfig.configFile,
+    bucket: awsConfig.bucket,
+    acl: 'public-read',
+    makeKey: function (req) {
+     var fileName = req.files.attachment.file.originalFilename;
+     fileName = moment.utc().format('YYYY-MM-DD-hh-mm-ss-') + fileName;
+     return 'uploadedImages/' + fileName;
+    },
+    respond: function (req, res, key) {
+     res.send(200, {
+       file: {
+         url: 'https://' + awsc.bucket + '.s3.amazonaws.com/' + key
+       }
+     });
     }
-  ]
-];
+  });
+}*/
